670f97309eeefed3260ff8158eb774ba8515ebf4
Brenner Haverlock <40365743+officialbrenner@users.noreply.github.com>
2018-08-01 16:01:47 -0400

    defined the lstm parameters
    also added regression layer

--- CryptoForcast.py
+++ CryptoForcast.py
@@ -227,6 +227,22 @@ train_inputs, train_outputs = [],[]
 for ui in range(num_unrolling):
     train_inputs.append(tf.placeholder(tf.float32, shape=[batch_size, D], name = 'train_inputs_%d'%ui))
     train_outputs.append(tf.placeholder(tf.float32, shape = [batch_size, 1], name = 'train_outputs%d'%ui))
+#init the lstm cells 
+lstm_cells = [
+        
+        tf.contrib.rnn.LSTMCell(num_units=num_node[ix],
+                                is_tuple = True,
+                                initializer = tf.contrib.layers.xavier_initializer()
+                                )
+        for ix in range(n_layers)]
+
+drop_lstm_cells = [tf.contrib.rnn.DropoutWrapper(
+        lstm, input_keep_prob=1.0, output_keep_prob= 1.0 - dropout, state_keep_prob= 1.0 - dropout) for lstm in lstm_cells]
+drop_multi_cell = tf.contrib.rnn.MultiRNNCell(drop_lstm_cells)
+multi_cell = tf.contrib.rnn.MultiRNNCell(lstm_cells)
+#Linear regression layer w and b 
+r_0 = tf.get_variable('w', shape=[num_node[-1], 1], initializer= tf.contrib.layers.xavier_initializer())
+r_1 = tf.get_variable('b', initializer= tf.random_uniform([1], -0.1, 0.1))
             
         
 
